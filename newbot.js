2d378383b31333c10ad84d43bd4f4bda0f1b755f
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-09-11 01:13:25 +0400

    Update newbot.js

--- newbot.js
+++ newbot.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 const CC = require('./command_create.js');
 const Command = CC.Command;
 
+
+
 var Commandss = new CC.Commands();
 var fs = require("fs");
 
-
-
 function commandIs(str, msg){
-    return msg.content.toLowerCase().startsWith("nm!" + str);
+    return msg.content.toLowerCase().startsWith("." + str);
 }
 
 function pluck(array) {
@@ -27,60 +27,58 @@ function hasRole(mem, role)
         return false;
     }
 }
-
-client.on('ready', () => {
-    client.user.setActivity("Relax", { type: 1});
-   
-
-});
-
-client.setInterval(function game()
+var vole = 1;
+var dispatchedr;
+client.setInterval(function play()
 {
-    var hours = new Date(Date.now()).getHours().toString()
-   var minutes = new Date(Date.now()).getMinutes().toString()
-if(hours == "--" && minutes == "--")//вместо -- ставить время 
-{
- 
-    client.users.forEach(cheli => cheli.send("test"))
-  
- 
-}
+    var server = client.guilds.get("351491707554103296")
+    var channel;
+    if(client.guilds.get("351491707554103296").channels != undefined)
+    {
+       channel = client.guilds.get("351491707554103296").channels.get("488779541171404841")
+        if(channel.members.size > 0 && !channel.members.map(h=>h.id).includes("399601970685673473"))
+        {
+            client.voice.joinChannel(client.guilds.get("351491707554103296").channels.get("488779541171404841"))
+            .then(connection => {
+        
+             dispatcher =  connection.playArbitraryInput('http://wargaming.fm/4');
+                 dispatcher.on("start", dw => {
 
-}, 60000)
-
-
-client.on('message', message =>
-{ 
+                 })
+              dispatcher.setVolume(vole)
+                })
+                .catch(console.log);
+        }
+        if(channel.members.size == 1 && channel.members.map(h=>h.id).includes("399601970685673473"))
+        {
+           client.guilds.get("351491707554103296").channels.get("488779541171404841").leave()
+        }
+      
+    }
     
-    if (message.channel.type !== 'text' || message.author.bot) return;
-if(message.content.toLowerCase().startsWith("привет"))
-{
-  
- message.channel.send("Привет!")
-}
+}, 3000)
+client.on('message', message => {
+    if(commandIs("vol", message))
+    {
+        var volme = parseInt(message.content.substring(5))
+        console.log(volme)
+        var volume;
+        if(volme>0 && volme <=200)
+        {
+            volume = volme/100
+            client.voiceConnections.map(g=>g.dispatcher.setVolume(volume))
 
-});
-client.on("guildMemberAdd", member =>
-{
-  let guild = member.guild;
-  if (guild.channels.filter(c => c.name.includes("chat")).first() !== undefined)
-  guild.channels.filter(c => c.name.includes("chat")).first().send(`Добро пожаловать, ${member}, на сервер **${guild.name}**!`/*Пример дефолтного приветствия*/);
+        }
+       
+        
+//
+    }
+   
+  });
 
-  /*
 
-  ${member} - ник пользователя
-  ${guild.name} - название сервера
-  <@${member.id}> - упоминание пользователя
 
-  */
 
-  
-  // Выдача роли
 
-  if(guild.roles.filter(r=>r.name.toLowerCase().includes(`пидр`)).first() !== undefined) {
-    var foundRole = guild.roles.filter(r=>r.name.toLowerCase().includes(`пидр`)).first();
-
-  member.addRole(foundRole.id)  } else {}
-})
 
 client.login(process.env.BOT_TOKEN);
