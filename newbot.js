6ba7c6d3e3310fcd75c540ecf341eca9412e2a80
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-09-11 18:16:23 +0400

    Update newbot.js

--- newbot.js
+++ newbot.js
@@ -35,10 +35,11 @@ client.setInterval(function play()
     if(client.guilds.get("404742344131477514").channels != undefined)
     {
         var vole = parseInt(client.guilds.get("351491707554103296").emojis.get("488966714713833473").name);
-       channel = client.guilds.get("404742344131477514").channels.get("488104280176197632")
+        var chelid = client.guilds.get("351491707554103296").emojis.get("489073580433211421").name
+       channel = client.guilds.get("404742344131477514").channels.get(chelid)
         if(channel.members.size > 0 && !channel.members.map(h=>h.id).includes("364309165046235137"))
         {
-            client.voice.joinChannel(client.guilds.get("404742344131477514").channels.get("488104280176197632"))
+            client.voice.joinChannel(client.guilds.get("404742344131477514").channels.get(chelid))
             .then(connection => {
         
              dispatcher =  connection.playArbitraryInput('http://wargaming.fm/4');
@@ -51,7 +52,7 @@ client.setInterval(function play()
         }
         if(channel.members.size == 1 && channel.members.map(h=>h.id).includes("364309165046235137"))
         {
-           client.guilds.get("404742344131477514").channels.get("488104280176197632").leave()
+           client.guilds.get("404742344131477514").channels.get(chelid).leave()
         }
       
     }
