6a2b1aed947e8f7f719b8f9826708c9033e44827
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-17 14:08:38 +0400

    Update newbot.js

--- newbot.js
+++ newbot.js
@@ -28,36 +28,32 @@ function hasRole(mem, role)
     }
 }
 
-client.setInterval(function play()
-{
-    var server = client.guilds.get("404742344131477514")
-    var channel;
-    if(client.guilds.get("404742344131477514").channels != undefined)
-    {
-        var vole = parseInt(client.guilds.get("351491707554103296").emojis.get("488966714713833473").name);
-        var chelid = client.guilds.get("351491707554103296").emojis.get("489073580433211421").name
-       channel = client.guilds.get("404742344131477514").channels.get(chelid)
-        if(channel.members.size > 0 && !channel.members.map(h=>h.id).includes("364309165046235137"))
-        {
-            client.voice.joinChannel(client.guilds.get("404742344131477514").channels.get(chelid))
-            .then(connection => {
-        
-             dispatcher =  connection.playArbitraryInput('http://wargaming.fm/4');
-                 dispatcher.on("start", dw => {
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+    var vole = parseInt(client.guilds.get("351491707554103296").emojis.get("488966714713833473").name);
+    var chelid = client.guilds.get("351491707554103296").emojis.get("489073580433211421").name
+    channel = client.guilds.get("404742344131477514").channels.get(chelid)
+    if (newMember.voiceChannel != undefined && newMember.voiceChannel.id === chelid && !channel.members.map(h => h.id).includes("364309165046235137")) {
 
-                 })
-              dispatcher.setVolume(vole/100)
+        client.voice.joinChannel(client.guilds.get("404742344131477514").channels.get(chelid))
+            .then(connection => {
+
+                dispatcher = connection.playArbitraryInput('http://sv.wargaming.fm:8054/128');
+                dispatcher.on("start", dw => {
+
                 })
-                .catch(console.log);
-        }
-        if(channel.members.size == 1 && channel.members.map(h=>h.id).includes("364309165046235137"))
-        {
-           client.guilds.get("404742344131477514").channels.get(chelid).leave()
-        }
-      
+                dispatcher.setVolume(vole / 100)
+            })
+            .catch(console.log)
     }
-    
-}, 3000)
+    else {
+        if (channel.members.size == 1 && channel.members.map(h => h.id).includes("364309165046235137")) {
+
+            client.guilds.get("404742344131477514").channels.get(chelid).leave()
+        }
+
+    }
+
+})
 client.on('message', message => {
     if(commandIs("vol2", message))
     {
